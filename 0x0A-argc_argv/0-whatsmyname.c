0df072fd684dc5d6e84359320c5b12b9597dfa76
Mohamed Salah <52123133+Mohamed-Elrayes@users.noreply.github.com>
2023-09-01 20:45:48 +0300

    Update 0-whatsmyname.c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,15 +1,16 @@
-#include <stdio.h>
 #include "main.h"
+#include <stdio.h>
+
 /**
- * main - prints the name of the program
- * @argc: number of arguments
- * @argv: array of arguments
+ * main - this program prints its name
  *
- * Return: Always 0 (Success)
+ * @argc: number of arguments
+ * @argv: string of arguments
+ *
+ * Return: 0
  */
 int main(int argc __attribute__((unused)), char *argv[])
 {
 	printf("%s\n", argv[0]);
-
 	return (0);
 }
