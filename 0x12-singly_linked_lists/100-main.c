ec733b532fece10102818e14fdcd03a10b317961
Mohamed Salah <52123133+Mohamed-Elrayes@users.noreply.github.com>
2023-09-22 19:50:24 +0300

    Update 100-main.c

--- 0x12-singly_linked_lists/100-main.c
+++ 0x12-singly_linked_lists/100-main.c
@@ -1,10 +1,4 @@
 #include <stdio.h>
-
-/**
- * main - check the code
- *
- * Return: Always 0.
- */
 int main(void)
 {
     printf("(A tortoise, having pretty good sense of a hare's nature, challenges one to a race.)\n");
