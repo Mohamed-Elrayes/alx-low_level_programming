80fac1a565ff8df3876a250e425e963449630092
Mohamed Salah <52123133+Mohamed-Elrayes@users.noreply.github.com>
2023-09-22 19:48:49 +0300

    Update 4-free_list.c

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -1,14 +1,9 @@
 #include <stdlib.h>
 #include "lists.h"
 
-/**
- * free_list - frees a linked list
- * @head: list_t list to be freed
- */
 void free_list(list_t *head)
 {
 	list_t *temp;
-
 	while (head)
 	{
 		temp = head->next;
@@ -17,4 +12,3 @@ void free_list(list_t *head)
 		head = temp;
 	}
 }
-
